93297212da3e186e6b5409738fb6c4e3d743e855
ticigomezgit-tech <tomasgomezarriola01@gmail.com>
2025-08-26 17:27:01 -0300

    Subo ejercicios Unidad 1

---
+++ Unidad 1/Ejercicios Unidad 1.py
@@ -0,0 +1,70 @@
+#Trabajo unidad 1
+print("---Actividad 1---")
+print("Hola Mundo!")
+print("---Actividad 2---")
+nombre=input("¿cual es tu nombre?")
+print(f"Hola {nombre}")
+print("---Actividad 3---")
+nombre=input("Dime tu nombre") 
+apellido=input("Ahora dime tu apellido") 
+edad=int(input("¿Que edad tienes?")) 
+lugar=input("Ingresa su residencia") 
+print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {lugar}")
+print("---Actividad 4---")
+radio=int(input("Ingrese el radio del circulo"))
+area=3,14*radio**2
+perimetro=2*3,14*radio
+print(f"el area del circulo es {area} y el perimetro es {perimetro}")
+print("---Actividad 5---")
+seg=int(input("Ingrese una cantidad de segundos"))
+horas=seg/3600
+print(f"Los {seg} s, equivalen a {horas} hs")
+print("---Actividad 6---")
+num=int(input("ingrese un numero"))
+tabla=num*1
+print(tabla)
+tabla=num*2
+print(tabla)
+tabla=num*3
+print(tabla)
+tabla=num*4
+print(tabla)
+tabla=num*5
+print(tabla)
+tabla=num*6
+print(tabla)
+tabla=num*7
+print(tabla)
+tabla=num*8
+print(tabla)
+tabla=num*9
+print(tabla)
+tabla=num*10
+print(tabla)
+print("---Actividad 7---")
+n1=int(input("ingrese el primer numero"))
+n2=int(input("ingrese el segundo numero"))
+suma=n1+n2
+print(f"la suma es {suma}")
+resta=n1-n2
+print(f"la resta es {resta}")
+mult=n1*n2
+print(f"la multiplicacion es {mult}")
+div=n1/n2
+print(f"la division es {div}")
+print("---Actividad 8---")
+altura=float(input("ingrese su altura"))
+peso=int(input("ingrese su peso"))
+imc=peso/altura**2
+print(f"Su indice de masa corpral es {imc}")
+print("---Actividad 9---")
+celsius=int(input("ingrese los grados celsius"))
+fahrenheit=(celsius*1.8)+32
+print(f"Su equivalente en grados fahrenheit es {fahrenheit} grados")
+print("---Actividad 10---")
+n1=int(input("ingrese el primer numero"))
+n2=int(input("ingrese el segundo numero"))
+n3=int(input("ingrese el tercer numero"))
+suma=n1+n2+n3
+promedio=suma/3
+print(f"el promedio de los tres numeros ingresados es de {promedio}")
