cc4addaeb5f6fd7e52833eef02e6a795d42d21df
ticigomezgit-tech <tomasgomezarriola01@gmail.com>
2025-08-29 21:00:42 -0300

    TP 3 resuelto

--- Unidad 1/Estructuras Condicionales/tp3.py
+++ Unidad 1/Estructuras Condicionales/tp3.py
@@ -31,17 +31,76 @@ contraseña=input("ingrese su contraseña: ")
 if len(contraseña)>=longitud_minima and len(contraseña)<=longitud_maxima:
     print("Ha ingresado una contraseña correcta")
 else:
-    print("Por favor ingrese una contraseña de netre 8 y 14 caracteres")
+    print("Por favor ingrese una contraseña de entre 8 y 14 caracteres")
 #Actividad 6
 import random
 numeros_aleatorios=[random.randint(1, 100) for i in range(50)]
 from statistics import mode, median, mean
 if mean(numeros_aleatorios)>median(numeros_aleatorios):
     if median(numeros_aleatorios)>mode(numeros_aleatorios):
+        print(numeros_aleatorios)
         print("sesgo positivo")
 elif mean(numeros_aleatorios)<median(numeros_aleatorios):
     if median(numeros_aleatorios)<mode(numeros_aleatorios):
+        print(numeros_aleatorios)
         print("sesgo negativo")
 elif mean(numeros_aleatorios)==median(numeros_aleatorios):
     if median(numeros_aleatorios)==mode(numeros_aleatorios):
+        print(numeros_aleatorios)
         print("sin sesgo")
+#Actividad 7
+frase=input("ingrese una frase: ")
+if frase[-1] in ("AEIOUaeiou"):
+    print(frase,"!")
+else:
+    print(frase)
+#Actividad 8
+nombre=input("ingrese su nombre: ")
+opcion=int(input("ingrese 1, si desea su nombre en mayusculas, ingrese 2 si desea su nombre en minusculas, y ingrese 3 si desea su numbre con la primer letra en mayusculas: "))
+if opcion==1:
+    nombre=nombre.upper()
+    print(nombre)
+elif opcion==2:
+    nombre=nombre.lower()
+    print(nombre)
+elif opcion==3:
+    nombre=nombre.title()
+    print(nombre)
+else:
+    print("Por favor ingrese 1, 2 o 3")
+#Actividad 9
+magnitud=float(input("ingrese la magnitud del terremoto: "))
+if magnitud<3:
+    print("Muy leve")
+elif magnitud>=3 and magnitud<4:
+    print("Leve")
+elif magnitud>=4 and magnitud<5:
+    print("Moderado")
+elif magnitud>=5 and magnitud<6:
+    print("Fuerte")
+elif magnitud>=6 and magnitud<7:
+    print("Muy fuerte")
+elif magnitud>=7:
+    print("Extremo")
+#Actividad 10
+hemisferio=input("ingrese su hermisferio (N/S): ")
+mes=int(input("ingrese el numero del mes: "))
+dia=int(input("ingrese el numero del dia: "))
+if hemisferio=="n" or hemisferio=="N":
+    if (mes==3 and dia>=21) or (mes==4) or (mes==5) or (mes==6 and dia<=20):
+        print("Es primavera")
+    elif (mes==6 and dia>=21) or (mes==7) or (mes==8) or (mes==9 and dia<=20):
+        print("Es verano")
+    elif (mes==9 and dia>=21) or (mes==10) or (mes==11) or (mes==12 and dia<=20):
+        print("Es otoño")
+    elif (mes==12 and dia>=21) or (mes==1) or (mes==2) or (mes==3 and dia<=20):
+        print("Es invierno")
+if hemisferio=="s" or hemisferio=="S":
+    if (mes==3 and dia>=21) or (mes==4) or (mes==5) or (mes==6 and dia<=20):
+        print("Es otoño")
+    elif (mes==6 and dia>=21) or (mes==7) or (mes==8) or (mes==9 and dia<=20):
+        print("Es invierno")
+    elif (mes==9 and dia>=21) or (mes==10) or (mes==11) or (mes==12 and dia<=20):
+        print("Es primavera")
+    elif (mes==12 and dia>=21) or (mes==1) or (mes==2) or (mes==3 and dia<=20):
+        print("Es verano") 
